7e02f2a583ede80dd7cab115a1836361fc366917
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-05-06 15:47:03 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    for($i = 0; $i < count($hotels); $i++){
+        $hotel = $hotels[$i];
+        $name = $hotel['name'];
+        $description = $hotel['description'];
+        $parking = $hotel['parking'];
+        $vote = $hotel['vote'];
+        $center_distance = $hotel['distance_to_center'];
+        $text = "-Nome: $name -Descrizione: $description  -Parcheggio: $parking  -Voto: $vote  -Distanza dal centro: $center_distance";
+        // var_dump($hotel);
+        ?>
+          <p> <?php echo $text ?></p>
+        <?php
+    }
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel</title>
+</head>
+<body>
+    
+    
+ 
+    
+</body>
+</html>
